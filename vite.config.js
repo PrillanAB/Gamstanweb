3caa503d50ace41c3138d2839112c115bfd2bf5d
Axel Hildebrand <axelhildebrand@axelsmac.local>
2025-05-13 11:59:20 +0200

    nej

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/threet2/',  // Replace with your repository name
+  base: '/Gamstanweb/',  // Replace with your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
